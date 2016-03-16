16ed643fb3e4dd5e3463f648c09387bad3c0baf6
Tom Crockett <pelotom@gmail.com>
2016-03-16 07:08:01 -0700

    Use string interpolation because why not

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const λ = ({raw}, ...subs) => {
   const expr = raw
-    .reduce((prev, next, n) => prev + '(___subs[' + (n - 1) + '])' + next)
-    .replace(/#(\d+)/g, (_, n) => '(___args[' + n + '])')
+    .reduce((prev, next, n) => prev + `(___subs[${n - 1}])` + next)
+    .replace(/#(\d+)/g, (_, n) => `(___args[${n}])`)
 
   const evaluate = new Function('___subs', '___args', 'return (' + expr + ');')
 
