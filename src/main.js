cf3f2a79a8e2ad169004944e67fdcb5247b0b5b2
Tom Crockett <pelotom@gmail.com>
2016-03-16 06:47:12 -0700

    Need to use variable interpolation after all

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default ({raw: [raw]}) => eval('(function (...args) { return (' +
-  raw.replace(/#(\d+)/g, (_, n) => '(args[' + n + '])') +
-'); })')
+const λ = ({raw}, ...subs) => {
+  const expr = raw
+    .reduce((prev, next, n) => prev + '(___subs[' + (n - 1) + '])' + next)
+    .replace(/#(\d+)/g, (_, n) => '(___args[' + n + '])')
+
+  const evaluate = new Function('___subs', '___args', 'return (' + expr + ');')
+
+  return (...args) => {
+    return evaluate(subs, args)
+  }
+}
+
+export default λ
