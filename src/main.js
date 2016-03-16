aff5397e8ef3f2b59d83dc63661fa3e8d93361b5
Tom Crockett <pelotom@gmail.com>
2016-03-16 07:09:06 -0700

    More string interpolation

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const λ = ({raw}, ...subs) => {
     .reduce((prev, next, n) => prev + `(___subs[${n - 1}])` + next)
     .replace(/#(\d+)/g, (_, n) => `(___args[${n}])`)
 
-  const evaluate = new Function('___subs', '___args', 'return (' + expr + ');')
+  const evaluate = new Function('___subs', '___args', `return (${expr});`)
 
   return (...args) => {
     return evaluate(subs, args)
